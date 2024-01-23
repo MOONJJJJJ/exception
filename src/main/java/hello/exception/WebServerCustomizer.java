70346aa06345dfee30e4e142ddde6d4b31f8dc02
MOONJJJJJ <yangmoonjj5@gmail.com>
2024-01-23 16:02:45 +0900

    9 - HandlerExceptionResolver 시작

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -1,13 +1,20 @@
 package hello.exception;
 
+import hello.exception.resolver.MyHandlerExceptionResolver;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.http.HttpStatus;
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
-@Component
+import java.util.List;
+
+//@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+
+
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
         ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/errorpage/404");
